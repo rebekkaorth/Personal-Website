76b5f7b6d491ff307a3c854f694ba754dc224255
bekki094 <bekki094@gmail.com>
2017-09-10 18:55:20 +0100

    scrolling effect - content expansion

--- script.js
+++ script.js
@@ -2,15 +2,30 @@
  * Created by rebekkaorth on 22.05.17.
  */
 
+
 'use strict';
 
-$(document).ready(() => {
+$(document).ready(function(){
 
-$('.content').hide();
+    $('.scrollMe').click(function(e){
+        e.preventDefault();
 
-$('button').click((event)=>{
-   $('.content').slideDown();
-});
+        var target = this.hash;
+        var $target = $(target);
+
+        let percentage = 0.01 ;
+        // console.log(($(document).height()));
+        if (($(document).width() < 574)) {
+            percentage = 0.036;
+            // console.log(($(document).height() * percentage));
+        }
+
+        $('body, html').stop().animate({
+            'scrollTop': $(target).offset().top - ($(document).height() * percentage)
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
 
 
 });
